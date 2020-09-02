b0034d3a3133a88e164464a145c7acbdc8df59bf
Francisco Cerda <francisco.cerda@gmail.com>
2020-09-02 12:17:05 -0400

    project babel+webpack

---
+++ edutecno/m3.2/01-S1/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "none",
+  entry: "./src/check.js",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      },
+    ],
+  },
+};
